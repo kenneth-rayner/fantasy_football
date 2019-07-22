5a73f7ac6e5343a97324c82f1f94beb4cc6ba361
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2019-07-22 09:35:27 +0100

    wip

--- app/controllers/PlayerManagerController.scala
+++ app/controllers/PlayerManagerController.scala
@@ -3,8 +3,10 @@ package controllers
 import javax.inject.Inject
 import models.Player
 import play.api.libs.json.Json
-import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
 import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.api.collections.bson.BSONCollection
+import reactivemongo.bson.BSONDocument
 import reactivemongo.play.json.ImplicitBSONHandlers.JsObjectDocumentWriter
 import reactivemongo.play.json.collection._
 
@@ -17,17 +19,17 @@ class PlayerManagerController @Inject()(cc: ControllerComponents, mongo: Reactiv
   private def collection: Future[JSONCollection] =
     mongo.database.map(_.collection[JSONCollection]("players"))
 
-  private def get(name: String): Future[Option[Player]] =
+  private def get(_id: String): Future[Option[Player]] =
     collection.flatMap(_.find(
-      Json.obj("name" -> name),
+      Json.obj("_id" -> _id),
       None
     ).one[Player])
 
 
-  def getPlayer(name: String) = Action.async {
+  def getPlayerById(_id: String) = Action.async {
     implicit request: Request[AnyContent] =>
 
-      get(name).map(
+      get(_id).map(
         _.map(player => Ok(Json.toJson(player))).getOrElse(NotFound)
       )
   }
@@ -38,9 +40,14 @@ class PlayerManagerController @Inject()(cc: ControllerComponents, mongo: Reactiv
       collection.flatMap(_.insert.one(request.body).map(_ => Ok))
   }
 
-  def deletePlayer = Action.async(parse.json[Player]) {
+  def deletePlayerByBody = Action.async(parse.json[Player]) {
     implicit request: Request[Player] =>
       collection.flatMap(_.delete.one(request.body).map(_ => Ok))
+  }
+
+  def deletePlayerById(id: String) = Action.async {
+    implicit request =>
+      collection.flatMap(_.delete.one(Json.obj("_id" -> id)).map(_ => Ok("Success")))
   }
 
   def deletePlayerByName(name: String) = Action.async {
@@ -48,24 +55,16 @@ class PlayerManagerController @Inject()(cc: ControllerComponents, mongo: Reactiv
       collection.flatMap(_.delete.one(Json.obj("name" -> name)).map(_ => Ok("Success")))
   }
 
-  def updatePlayerByName(name: String) = Action.async {
-    implicit request =>
-      collection.flatMap(_.update.one(
-        q = Json.obj("name" -> name),
-        u = Json.obj("name" -> "Ken")
-      ).map(_ => Ok("Success")))
-  }
-
-  def updateName(name: String, newName: String) = Action.async {
+  def updatePlayerName(_id: String, newName: String) = Action.async {
     implicit request =>
 
-      get(name).map(_.get).map(
+      get(_id).map(_.get).map(
         result =>
           collection.flatMap(_.update.one(
-            q = Json.obj("name" -> name),
+            q = Json.obj("_id" -> _id),
             u = Json.obj(
               "name" -> newName,
-              "id" -> result.id,
+              "id" -> result._id,
               "value" -> result.value
             )
           ))
@@ -73,50 +72,52 @@ class PlayerManagerController @Inject()(cc: ControllerComponents, mongo: Reactiv
 
   }
 
-  def updateValue(name: String, newValue: Int) = Action.async {
+  def updateValue(_id: String, newValue: Int) = Action.async {
     implicit request =>
 
-      get(name).map(_.get).map(
+      get(_id).map(_.get).map(
         result =>
           collection.flatMap(_.update.one(
-            q = Json.obj("name" -> name),
+            q = Json.obj("_id" -> _id),
             u = Json.obj(
               "name" -> result.name,
-              "id" -> result.id,
+              "_id" -> result._id,
               "value" -> newValue
             )
           ))
       ).map(_ => Ok("Success"))
   }
 
-  def increaseValue(name: String, increase: Int) = Action.async {
+  def increaseValue(_id: String, increase: Int) = Action.async {
     implicit request =>
 
-      get(name).map(_.get).map(
+      get(_id).map(_.get).map(
         result =>
           collection.flatMap(_.update.one(
-            q = Json.obj("name" -> name),
+            q = Json.obj("_id" -> _id),
             u = Json.obj(
               "name" -> result.name,
-              "id" -> result.id,
+              "_id" -> result._id,
               "value" -> (result.value + increase)
             )
           ))
       ).map(_ => Ok("Success"))
   }
 
-  def decreaseValue(name: String, decrease: Int) = Action.async {
+  def decreaseValue(_id: String, decrease: Int) = Action.async {
     implicit request =>
 
-      get(name).map(_.get).map(
+      get(_id).map(_.get).map(
         result =>
           collection.flatMap(_.update.one(
-            q = Json.obj("name" -> name),
+
+            q = Json.obj("_id" -> _id),
             u = Json.obj(
               "name" -> result.name,
-              "id" -> result.id,
+              "id" -> result._id,
               "value" -> (result.value - decrease)
             )
+
           ))
       ).map(_ => Ok("Success"))
   }
--- app/models/Player.scala
+++ app/models/Player.scala
@@ -3,7 +3,7 @@ package models
 import play.api.libs.json._
 
 
-case class Player(id: String, name: String, value: Int)
+case class Player(_id: String, name: String, value: Int)
 
 object Player {
   implicit lazy val format: OFormat[Player] = Json.format
